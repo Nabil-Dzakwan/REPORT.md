02f7cf500b8f2577b78e0bb7c0f83c8a6ee646b4
Nabil-Dzakwan <muhammadnabildzakwan@gmail.com>
2025-09-13 13:58:11 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float Alas,Tinggi,Luas;
+    Alas =5;
+    Tinggi = 10;
+    Luas = 0,5*Alas*Tinggi;
+    keliling =
+    printf ("Luas Segitiga = %f",Luas);
+    return 0;
+}
